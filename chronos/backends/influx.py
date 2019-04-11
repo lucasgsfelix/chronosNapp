e830d9e5b9f71cfb1c4c5243f3eeeb7f689051ec
lucasgsfelix <lucasgsfelix@gmail.com>
2019-04-11 09:57:43 -0300

    Add files via upload

--- chronos/backends/influx.py
+++ chronos/backends/influx.py
@@ -1,18 +1,12 @@
 """InfluxDB backend"""
-
-import sys
-import time
 from datetime import datetime
 
 from influxdb import InfluxDBClient
-
-import settings
 
 from utils import Utils
 
 def now():
     return datetime.utcnow().isoformat()
-
 
 class InfluxBackend:
     """This Backend is responsible to the connection with InfluxDB."""
@@ -37,35 +31,20 @@ class InfluxBackend:
 
         self._write_endpoints(data)
 
-    #def delete(self, namespace, start_timestamp, end_timestamp=None):
-    #    """Delete the entire database --
-    #    start_timestamp and end_timestamp most be a timestamp"""
-    #    delete_start_query = ('DELETE FROM ' + namespace + ' WHERE time >' +
-    #                          str(start_timestamp))
-    #    if start_timestamp is not None:
-    #        delete_end_query = "' and time < '"+str(end_timestamp)+"'"
-    #        Utils.validate_timestamp(start_timestamp, end_timestamp)
-    #        self.client.query(delete_start_query+delete_end_query)
-    #    else:
-    #        self.client.query(delete_start_query)
+    def delete(self, namespace, start_timestamp=None, end_timestamp=None):
+        """Delete the entire database --
+        start_timestamp and end_timestamp most be a timestamp"""
+        self._validate_namespace(namespace)
+        utils_obj = Utils()
+        utils_obj.validate_timestamp(start_timestamp, end_timestamp)
+        self._delete_points(namespace, start_timestamp, end_timestamp)
 
-    #def get(self, namespace, start_timestamp=None, end_timestamp=None):
-    #    """Make a query to retrieve something in the database."""
-    #    if start_timestamp is None:
-    #        print("SELECT * FROM "+namespace)
-    #        print(self.params)
-    #        return self.client.query("SELECT * FROM "+namespace)
-
-    #    time_start_query = ("SELECT value FROM " + namespace + " WHERE time >'" +
-    #                        str(start_timestamp) + "'")
-    #    
-    #    if end_timestamp is not None:
-    #        time_end_query = " and time < '"+str(end_timestamp)+"'"
-    #        utils_obj = Utils()
-    #        utils_obj.validate_timestamp(start_timestamp, end_timestamp)
-    #        return self.client.query(time_start_query+time_end_query)
-    #    print("query ",time_start_query)
-    #    return self.client.query(time_start_query)
+    def get(self, namespace, start_timestamp=None, end_timestamp=None):
+        """Make a query to retrieve something in the database."""
+        self._validate_namespace(namespace)
+        utils_obj = Utils()
+        utils_obj.validate_timestamp(start_timestamp, end_timestamp)
+        self._get_points(namespace, start_timestamp, end_timestamp)
 
     def _read_config(self, settings):
         self._params = {}
@@ -90,19 +69,62 @@ class InfluxBackend:
         if not self._get_database() and create_database:
             self._create_database()
         else:
-            raise Exception("Database do not exists")
+            raise Exception("Database do not exists.")
 
         try:
-            self.client.write_points(data)
+            self._client.write_points(data)
         except Exception:
             raise Exception("Error inserting data to InfluxDB.")
 
     def _get_database(self):
         """Verify if a database exists"""
-        all_dbs = self.client.get_list_database()
+        all_dbs = self._client.get_list_database()
         exist = filter(lambda x: x['name'] == self._db_name, all_dbs)
         
         if exist:
             return exists[0]
 
         return False
+
+    def _delete_points(self, namespace, start_timestamp, end_timestamp):
+
+        query = {'query': 'DELETE FROM ' + namespace,
+                'start':" WHERE time > '" + str(start_timestamp)+"'"
+                'end':" and time < '"+str(end_timestamp)+"'"}
+
+        result_query = self._query_assemble(query)
+
+        self._client.query(delete_query)
+
+    def _get_points(self, namespace, start_timestamp, end_timestamp):
+
+        query = {'query': 'SELECT * FROM ' + namespace,
+                'start':" WHERE time > '" + str(start_timestamp)+"'"
+                'end':" and time < '"+str(end_timestamp)+"'"}
+        
+        result_query = self._query_assemble(query)
+
+        return self.client.query(time_start_query)
+
+    def _query_assemble(self, query):
+
+        if (not "'None'" in query['start']) and (not "'None'" in query['end']):
+            result_query = query['query'] + query['start'] + query['end']
+        elif not "'None'" in query['start']:
+            result_query = query['query'] + query['start']
+        elif not "'None'" in query['end']:
+            result_query = query['query'] + query['end']
+        else:
+            result_query = query['query']
+
+        return result_query
+
+    def _validate_namespace(namespace):
+
+        if namespace is None:
+            raise Exception("Invalid namespace.")
+        else:
+            all_nspace = self._client.get_list_measurements()
+            exist = filter(lambda x: x['name'] == self.namespace, all_nspace)
+            if not exist:
+                raise Exception("Required namespace does exist.")
